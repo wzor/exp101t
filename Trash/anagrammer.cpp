5c17b7be9141e233ef8b2365b12d105c789bf60e
exp101t <rittergerman2000@yandex.ru>
2017-06-04 17:56:39 +0700

    Create anagrammer.cpp

---
+++ Trash/anagrammer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool isSame(string a, string b){
+	int i, j = -1, count, m;
+		while(b[++j]){
+			i = -1; count = 0; m = j - 1;
+			for(int c = j - 1; c > -1; c--) if(b[j]==b[c]){count++; break;}
+			if(!count){
+				while(b[++m]) if(b[m]==b[j]) count++;
+				while(a[++i]) if(a[i]==b[j]) count--;
+				if(count) return false;
+			}
+		}return true;
+}
+
+int main(){
+	string a, b; int c = 0;
+	ifstream words("words.txt"); ofstream out("output.txt");
+	while(!words.eof()){
+		words >> a; ifstream list("wordlist.txt");
+		while(!list.eof()){
+			list >> b;
+			if(a.length()==b.length()) if(isSame(a, b)) if(c) out << ',' << b; else{c++; out << b;}
+		}list.close();
+	}words.close(); out.close();
+}
